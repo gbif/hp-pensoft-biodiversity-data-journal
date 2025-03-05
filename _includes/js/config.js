d7f77d808db980ddc9d1ce68edeeb418b6eef452
teodorgeorgiev <preprint@pensoft.net>
2025-03-05 16:27:54 +0200

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -41,8 +41,7 @@ var siteConfig = {
        peerReview: true
      }
   },
-  //rootFilter: {publishingOrganizationKey: [publisherKey]}
-  }
+  //rootFilter: {publishingOrganizationKey: [publisherKey]}  
 };
 
 // example of a language specific route overwrite
